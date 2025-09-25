af26ee072263e5e14a9fbcd10525e51aecacb5e6
Ulysse Pennetier <upennetier@gmail.com>
2025-09-25 10:21:15 +0200

    feat(exo_1.php): part 2

--- td_tp_poo/seance_g_synthese_des_concepts/exo_1.php
+++ td_tp_poo/seance_g_synthese_des_concepts/exo_1.php
@@ -25,3 +25,13 @@
   echo "Age : <b>{$person1->getAge()}</b>";
   echo '<br>';
   $person1->work(10);
+
+  echo "<br><h2>Etape 2</h2>";
+
+  $person2 = new Student('Fraichi', 'Sarah', 'femme', '9012345678B', 'TC', 18);
+	echo $person2->describe().'<br>';
+	$person2->work(20);
+	echo $person2->describe_with_money();
+	echo Student::getHumanNumber();
+
+  echo "<br><h2>Etape 3</h2>";
