16ef8bc057c2ef608f9c96c4af35518afef5ff6c
Ulysse Pennetier <upennetier@gmail.com>
2025-09-25 10:32:13 +0200

    feat(class): StudentMMI

---
+++ td_tp_poo/seance_g_synthese_des_concepts/classes/StudentMMI.php
@@ -0,0 +1,23 @@
+<?php
+  require_once 'Student.php';
+
+  final class StudentMMI extends Student {
+    private string $optionS4;
+
+    public function __construct(string $surname, string $name, string $sex, string $studentID, string $formation, int $age, string $optionS4) {
+      parent::__construct($surname, $name, $sex, $studentID, $formation, $age);
+      $this->changeOption($optionS4);
+    }
+
+    public function changeOption($optionS4): void {
+      $this->optionS4 = $optionS4;
+    }
+
+    public function describe_with_option(): string {
+      return parent::describe()."<li>Option : <b>{$this->optionS4}</b></li>";
+    }
+
+    public function describe_with_mmi(): string {
+      return parent::describe()."<li>Élève de : <b>MMI</b></li>";
+    }
+  }
